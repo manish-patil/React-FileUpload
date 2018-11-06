873bd7d2264b39122095d9c61442104ad8c2fdd4
Manish Patil <manish.patil@hotmail.com>
2018-11-06 13:50:07 +0530

    Initial Commit - React - FileUpload

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,31 @@ var path = require("path");
 
 module.exports = {
     entry: ["./src/index.tsx"],
+    mode: "development",
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/
+    },
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "public/dist"),
+        publicPath: '/dist'
+    },
+    devServer: {
+        historyApiFallback: true,
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]                
+        extensions: [".ts", ".tsx", ".js", ".json"]
     },
     module: {
-        loaders: [{
-            test: /\.tsx?$/,
-            loader: "ts-loader"
-        }]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            }, {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
     }
 };
